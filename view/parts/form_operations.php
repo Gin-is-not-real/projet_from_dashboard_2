bb58b8e5df736aa76f7351b0d668bc6f576619e3
Gin-is-not-real <ninapariat@live.fr>
2021-05-03 15:46:19 +0200

    start style for tables

--- view/parts/form_operations.php
+++ view/parts/form_operations.php
@@ -1,16 +1,69 @@
 <section id="sec-formulaire">
 
     <form id="form-operation-add" action="index.php?action=add-operation" method="post">
-        <header>
+        <!-- <header>
             <h4>add new operation</h4>
-        </header>
-        <section id="form-content">
-            <div>
+        </header> -->
+            <table id="table_operations">
+
+                <tr>
+                    <th>Date</th>
+                    <th>Floor</th>
+                    <th>Room</th>
+                    <th>Cost</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td> <input type="date" name="inp-date" value="<?php echo date('Y-m-d'); ?>" </td>
+                    <td> 
+                        <select name="sel-floor" id="sel-floor">
+                            <?php 
+                                foreach($GLOBALS['floors'] as $value) {
+                            ?>
+                                <option value="<?= $value ?> "> <?= $value ?> </option></br>;
+                            <?php 
+                                }
+                            ?>
+                        </select>
+                    </td>
+    
+                    <td> 
+                        <select name="sel-room" id="sel-room">
+                            <?php 
+                                foreach($GLOBALS['rooms'] as $value) {
+                            ?>
+                                <option value="<?= $value ?> "> <?= $value ?> </option></br>;
+                            <?php 
+                                }
+                            ?>
+                        </select>
+                    </td>
+                
+                    <td> 
+                        <select name="sel-cost" id="sel-cost">
+                            <?php 
+                                foreach($GLOBALS['cost'] as $value) {
+                            ?>
+                                <option value="<?= $value ?> "> <?= $value ?> </option></br>;
+                            <?php 
+                                }
+                            ?>
+                        </select> 
+                    </td>
+    
+                    <td id="td-actions">
+                        <input type="submit" name="sub-add-op" value="add">
+                    </td>
+
+            </table>
+
+            <!-- <div>
                 <label for="inp-date">Date </label>
                 <input type="date" name="inp-date" value="<?php echo date('Y-m-d'); ?>">
-            </div> 
+            </div>  -->
 
-            <div>
+            <!-- <div>
                 <label for="sel-floor">Floor </label>
                 <select name="sel-floor" id="sel-floor">
                     <?php 
@@ -21,9 +74,9 @@
                         }
                     ?>
                 </select>
-            </div>
+            </div> -->
 
-            <div>
+            <!-- <div>
                 <label for="sel-room">Room </label>
                 <select name="sel-room" id="sel-room">
                     <?php 
@@ -34,9 +87,9 @@
                         }
                     ?>
                 </select>
-            </div>
+            </div> -->
 
-            <div>
+            <!-- <div>
                 <label for="sel-cost">Cost </label>
                 <select name="sel-cost" id="sel-cost">
                     <?php 
@@ -47,13 +100,14 @@
                         }
                     ?>
                 </select>
-            </div>
+            </div> -->
 
-            <div>
+            <!-- <div>
                 <input type="submit" name="sub-add-op" value="add">
-            </div>
+            </div> -->
 
         </div>
     </form>
+
     </section>
 </section>
